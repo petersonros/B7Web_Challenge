4d761311ab82b6251069d61db2d740838eee3361
Peterson <petersonros@hotmail.com>
2022-01-23 21:54:32 -0300

    projeto arrasta e solta cncluído

--- arrasta/script.js
+++ arrasta/script.js
@@ -1,3 +1,9 @@
+let areas = {
+    a: null,
+    b: null,
+    c: null
+};
+
 document.querySelectorAll('.item').forEach(item => {
     item.addEventListener('dragstart', dragStart);
     item.addEventListener('dragend', dragEnd);
@@ -10,6 +16,9 @@ document.querySelectorAll('.area').forEach(area =>{
 });
 
 // neutral area
+document.querySelector('.neutralArea').addEventListener('dragover', dragOverNeutral);
+document.querySelector('.neutralArea').addEventListener('dragleave', dragLeaveNeutral);
+document.querySelector('.neutralArea').addEventListener('drop', dropNeutral);
 
 // function item
 function dragStart(e){
@@ -32,13 +41,51 @@ function dragLeave(e){
     e.currentTarget.classList.remove('hover');
 }
 
+// drop nas caixinhas
 function drop(e){
     e.currentTarget.classList.remove('hover');
 
     if(e.currentTarget.querySelector('.item')=== null){
         let dragItem = document.querySelector('.item.dragging');
         e.currentTarget.appendChild(dragItem);
+        updateAreas();
     }
 }
 
 // function neutral area
+function dragOverNeutral(e) {
+    e.preventDefault();
+    e.currentTarget.classList.add('hover');
+}
+
+function dragLeaveNeutral(e){
+    //e.preventDefault();
+    e.currentTarget.classList.remove('hover');
+}
+
+// drop na origem
+function dropNeutral(e) {
+    e.currentTarget.classList.remove('hover');
+    let dragItem = document.querySelector('.item.dragging');
+    e.currentTarget.appendChild(dragItem);
+    updateAreas();
+}
+
+// Logic Functions
+function updateAreas() {
+    document.querySelectorAll('.area').forEach(area => {
+        let name = area.getAttribute('data-name');
+
+        if(area.querySelector('.item') !== null) {
+            areas[name] = area.querySelector('.item').innerHTML;
+        } else {
+            areas[name] = null;
+        }
+    });
+
+    if(areas.a === '1' && areas.b === '2' && areas.c === '3') {
+        document.querySelector('.areas').classList.add('correct');
+    } else {
+        document.querySelector('.areas').classList.remove('correct');
+    }
+}
