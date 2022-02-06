6eac11df1db73ab29d876113400212f105d1bea4
Peterson <petersonros@hotmail.com>
2022-02-05 22:31:58 -0300

    função limpa tela implementada

--- 05_desenho/script.js
+++ 05_desenho/script.js
@@ -1,5 +1,8 @@
 // Initial Data
 let currentColor = 'black';
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
 
 let screen = document.querySelector('#tela');
 let ctx = screen.getContext('2d');
@@ -12,24 +15,50 @@ document.querySelectorAll('.colorArea .color').forEach(item => {
 screen.addEventListener('mousedown', mouseDownEvent);
 screen.addEventListener('mousemove', mouseMoveEvent);
 screen.addEventListener('mouseup', mouseUpEvent);
+document.querySelector('.clear').addEventListener('click', clearScreen);
 
 // Functions
 function colorClickEvent(e){
     let color = e.target.getAttribute('data-color');
     currentColor = color;
-
     document.querySelector('.color.active').classList.remove('active');
     e.target.classList.add('active');
 }
 
-function mouseDownEvent(){
-    console.log("clicou o mouse");
+function mouseDownEvent(e){
+    canDraw = true;
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
 }
 
-function mouseMoveEvent(){
-    console.log("moveu o mouse");
+function mouseMoveEvent(e){
+    if(canDraw){
+        draw(e.pageX, e.pageY);
+    }
 }
 
 function mouseUpEvent(){
-    console.log("soltou o mouse");
+    canDraw = false;
+}
+
+function draw(x, y){
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = "round";
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.closePath();
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
+
+    mouseX = pointX;
+    mouseY = pointY;
+}
+
+function clearScreen() {
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
